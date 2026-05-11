86eb3be95dd83e465037e352a3e32b565c274c06
forgejo-actions <forgejo-actions@git.blastbrean.com>
2026-05-11 16:20:33 +0000

    Release v1.1.3

--- Menu.lua
+++ Menu.lua
@@ -56,7 +56,7 @@ local renderStepped = Signal.new(runService.RenderStepped)
 local menuMaid = Maid.new()
 
 -- Constants.
-local VERSION = "1.1.1"
+local VERSION = "1.1.3"
 local WATERMARK_TITLE = "Lycoris"
 Menu.VERSION = VERSION
 
